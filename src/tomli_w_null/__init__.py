0c8caa86c3ace1e4e9428889f6941d4c5b534d30
asrelo <47759736+asrelo@users.noreply.github.com>
2026-05-02 17:46:06 +0300

    Bump version: 0.1.1 → 0.2.0

--- src/tomli_w_null/__init__.py
+++ src/tomli_w_null/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2021 Taneli Hukkinen
 
 __all__ = ("dumps", "dump", "TOMLVersion", "TOML_VERSION_DEFAULT")
-__version__ = "0.1.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.2.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 from tomli_w_null._version import TOML_VERSION_DEFAULT, TOMLVersion
 from tomli_w_null._writer import dump, dumps
